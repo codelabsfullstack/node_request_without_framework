873c07952ff76eef619e0530e447521134a6bb24
codelabsfullstack <codelabsfullstack@gmail.com>
2018-09-03 18:33:26 -0500

    parse

---
+++ server.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const { parse } = require('querystring');
+
+
+const server = http.createServer((req, res) => {
+	if(req.method === 'POST') {
+		let body = '';
+		req.on('data', chunk => {
+			body += chunk.toString()  // convert Buffer to string
+			// console.log(chunk.toString())
+		});
+		req.on('end', () => {
+			console.log(parse(body));
+			res.end('ok');
+		})
+	} else {
+		res.end(`
+	      <!doctype html>
+	      <html>
+	      <body>
+	          <form action="/" method="post">
+	              <input type="text" name="fname" /><br />
+	              <input type="number" name="age" /><br />
+	              <input type="file" name="photo" /><br />
+	              <button>Save</button>
+	          </form>
+	      </body>
+	      </html>
+	  `);
+		
+	}
+})
+
+server.listen(2018);
